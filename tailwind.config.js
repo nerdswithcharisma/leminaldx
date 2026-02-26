c4411f21a8cfd7582effcf5a53c8523180c46aba
Brian Dausman <briandausman@gmail.com>
2026-02-26 14:37:34 -0600

    [header] style header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,17 @@
 module.exports = {
   content: [
     './wp-content/themes/leminaldx-child/**/*.{php,js,jsx}',
-    './src/**/*.{js,jsx,php}',
+    './src/**/*.{js,jsx,php,scss}',
   ],
   theme: {
     extend: {
       colors: {
         'brand-primary': '#000000',
         'brand-secondary': '#ffffff',
+        'leminal-border': '#D1CE9E',
+        'leminal-primary': '#233B9D',
+        'leminal-arrow': '#919DCE',
+        'primary-light': '#099EC9',
       },
     },
   },
