5a6efd24fa3133dc3977be37b4fbd71eeeca2fd4
Brian Dausman <briandausman@gmail.com>
2026-03-11 12:35:37 -0500

    [template] internal template

---
+++ wp-content/themes/leminaldx-child/page-internal.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Internal Pages
+ * Description: Template for internal/standard content pages.
+ */
+get_header();
+?>
+<main id="main" class="site-main site-main--internal">
+    <?php
+    while (have_posts()) :
+        the_post();
+        ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+        </article>
+        <?php
+    endwhile;
+    ?>
+</main>
+<?php
+get_footer();
