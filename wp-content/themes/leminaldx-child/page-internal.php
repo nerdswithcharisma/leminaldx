ec9bfdb652be63627cbefdbc855888d62cff59d0
Brian Dausman <briandausman@gmail.com>
2026-03-11 12:57:42 -0500

    [template] internal page template

--- wp-content/themes/leminaldx-child/page-internal.php
+++ wp-content/themes/leminaldx-child/page-internal.php
@@ -4,20 +4,23 @@
  * Description: Template for internal/standard content pages.
  */
 get_header();
-?>
-<main id="main" class="site-main site-main--internal">
-    <?php
-    while (have_posts()) :
-        the_post();
-        ?>
+
+while (have_posts()) :
+    the_post();
+    $thumb_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+    $underlay_style = $thumb_url
+        ? 'background-image: url(' . esc_url($thumb_url) . ');'
+        : 'background-color: #233b9d;';
+    ?>
+    <div class="leminal-internal-underlay" style="<?php echo $underlay_style; ?>"></div>
+    <main id="leminal-internal-content" class="site-main site-main--internal">
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <div class="entry-content">
                 <?php the_content(); ?>
             </div>
         </article>
-        <?php
-    endwhile;
-    ?>
-</main>
-<?php
+    </main>
+    <?php
+endwhile;
+
 get_footer();
